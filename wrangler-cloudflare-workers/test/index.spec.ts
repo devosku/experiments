fdf4f54e71ad536698e04dad33855ef760d3ec76
Oskari Pöntinen <mail.morko@gmail.com>
2024-05-04 12:04:22 +0300

    Fix tests

--- wrangler-cloudflare-workers/test/index.spec.ts
+++ wrangler-cloudflare-workers/test/index.spec.ts
@@ -7,19 +7,40 @@ import worker from '../src/index';
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
+const expectedResponse = `
+<!doctype html>
+<html>
+  <head>
+	<title>Example Cloudflare worker!</title>
+  </head>
+  <body>
+	<p>Enjoy your random image!</p>
+	<img src="https://source.unsplash.com/random/200x200?sig=1" />
+  </body>
+</html>
+`;
+
+describe('Test example worker', () => {
+	it('Responds with status 200', async () => {
+		const request = new IncomingRequest('https://example.com');
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		await waitOnExecutionContext(ctx);
+		expect(response.status).toEqual(200);
+	});
+
+	it('Responds with correct HTML (unit style)', async () => {
 		const request = new IncomingRequest('http://example.com');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatch(expectedResponse);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
+	it('responds with correct HTML (integration style)', async () => {
 		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatch(expectedResponse);
 	});
 });
